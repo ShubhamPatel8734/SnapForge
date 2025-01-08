f21b17c9b6b66407922e927265a37a6862282f82
Shubham Patel <164138722+ShubhamPatel8734@users.noreply.github.com>
2025-01-09 02:13:24 +0530

    Try a function by Input.html
    create template folder in UserApp & add it in settings.py; Temporarily create input.html form and 2 functions & urls to check working.

--- User/views.py
+++ User/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "input.html", {})
+
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# def show(request):
+#     if request.method == "POST":
+#
+#         im = request.FILES['img']
+#
+#         img = mpimg.imread(im)
+#
+#         plt.imshow(img)
+#         plt.axis('off')
+#         plt.show()
+
