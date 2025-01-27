77217ff828a8825d5714317ca2ab93cfe2fe22f9
Shubham Patel <164138722+ShubhamPatel8734@users.noreply.github.com>
2025-01-27 18:06:26 +0530

    Global var for session

--- User/views.py
+++ User/views.py
@@ -10,9 +10,97 @@ from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
-def index(request):
+email = ""
 
-    email = "pshubham8734@gmail.com"
+
+def signup(request):
+    if request.method == "POST":
+
+        name = request.POST['user_name']
+        user_email = request.POST['user_email']
+
+        records = {
+            "user_name": name,
+            "user_email": user_email
+        }
+        print("Form Data :", records)
+
+        try:
+            user_collection.insert_one(records)
+            return render(request, "Login.html", {})
+        except DuplicateKeyError:
+            err_msg = "The email address is already registered. Please use a different email."
+            print("-+-+-+-+-+-+-+-+-+-+-+- :- ", err_msg)
+            return render(request, "SignUp.html", {"error": err_msg})
+        except Exception as e:
+            print("-=-=-=-=-=-=-=-=-=-=-=- Somthing want wrong :- ", e)
+            err_msg = "An unexpected error occurred. Please try again."
+            return render(request, "SighUp.html", {"error": err_msg})
+
+    return render(request, "SignUp.html", {})
+
+
+from django.core.mail import send_mail
+import random
+
+
+def sendotp(request):
+    if request.method == 'POST':
+        otp = random.randint(1000, 9999)
+
+        user_email = request.POST['user_email']
+
+        print("*=*=*=*=*=*=* otp:- ",otp,"email:- ",user_email)
+
+        user = user_collection.update_one(
+            {"user_email": user_email},  # Filter: Match the email
+            {"$set": {"otp": str(otp)}}  # Update: Add or update the OTP field
+        )
+
+        if user.matched_count == 0:
+            err_msg = "Email not found. Please SignUp before requesting an OTP."
+            print("-=-=-=-=-=-=-=-", err_msg)
+            return render(request, "SignUp.html", {"error": err_msg})
+
+        global email
+        email = user_email
+
+        subject = 'Your SnapForge Portal OTP'
+        message = 'Dear user, you have attempted to login in the SnapForge Portal.\n\nUse OTP: ' + str(otp) + '\n\nNote: Do not share the OTP with anyone else. '
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email, ]
+
+        send_mail(subject, message, email_from, recipient_list)
+        print("+=+=+=+=+=+=+=+=+=+  " + email + "  +=+=+=+  Email has been sent successfully...")
+
+        print(f"*=*=*=*=*=*=*=*=*=*=*=*  OTP {otp} successfully added to the collection for email {email}")
+
+    return render(request, "Login.html", {"visibility": 'block'})  # Here needs to change.
+
+
+def login(request):
+    if request.method == 'POST':
+
+        # email = request.POST['user_email']
+        otp = request.POST['otp']
+
+        # print("+-+-+-+-+-+ Email from Send OTP Form :-", email)
+        user = user_collection.find_one({"user_email": email})
+
+        # print("User details :- ", user)
+
+        if user:
+            if user.get("otp") == otp:
+                print("*=*=*=*=*=*=*=*=*=*=*=* :- login successfully..")
+                return redirect("/Index")
+            else:
+                print("+-+-+-+-+-+-+-+-+-+-+-+ :- OTP is incorrect.")
+                return render(request, "Login.html", {"visibility": 'block', "error": "Invalid OTP."})  # Here needs to change.
+
+    return render(request, "Login.html", {})
+
+
+def index(request):
 
     user = user_collection.find_one({"user_email": email})
     print("User :-", user)
@@ -69,17 +157,17 @@ def img_creation(request):
             # File_3D = request.FILES['3D_File']  # function return a 3D file.
 
 
-            Left_path = fs.save(Left.name, Left)
-            Right_path = fs.save(Right.name, Right)
-            Front_path = fs.save(Front.name, Front)
-            Back_path = fs.save(Back.name, Back)
-            # File_3D_path = fs.save(File_3D.name, File_3D)
+            Left_path = fs.save(r_name(Left.name), Left)
+            Right_path = fs.save(r_name(Right.name), Right)
+            Front_path = fs.save(r_name(Front.name), Front)
+            Back_path = fs.save(r_name(Back.name), Back)
+            # File_3D_path = fs.save(r_name(File_3D.name), File_3D)
 
             # space" " replaces with "_" & "(",")" replaces with "".
-            Left_name = r_name(fs.get_valid_name(Left_path))
-            Right_name = r_name(fs.get_valid_name(Right_path))
-            Front_name = r_name(fs.get_valid_name(Front_path))
-            Back_name = r_name(fs.get_valid_name(Back_path))
+            Left_name = fs.get_valid_name(Left_path)
+            Right_name = fs.get_valid_name(Right_path)
+            Front_name = fs.get_valid_name(Front_path)
+            Back_name = fs.get_valid_name(Back_path)
             # File_3D_name = r_name(fs.get_valid_name(File_3D_path))
             File_3D_name = "Weird_Bird_3D.stl"
 
@@ -89,7 +177,7 @@ def img_creation(request):
                 "front_img": Front_name,
                 "back_img": Back_name,
                 "file_3D_img": File_3D_name,
-                "user_email": "pshubham8734@gmail.com"
+                "user_email": email
             }
             images_collection.insert_one(records)
             print(f":::::::::::::::::::  Left_name : {Left_name} \n:::::::::::::::::::  Right_name : {Right_name} \n:::::::::::::::::::  Front_name : {Front_name} \n:::::::::::::::::::  Back_name : {Back_name} \n:::::::::::::::::::  File_3D_img : {File_3D_name}")
@@ -98,89 +186,6 @@ def img_creation(request):
 
     return render(request, "Img_creation.html", {})
 
-
-def signup(request):
-    if request.method == "POST":
-
-        name = request.POST['user_name']
-        email = request.POST['user_email']
-
-        records = {
-            "user_name": name,
-            "user_email": email
-        }
-        print("Form Data :", records)
-
-        try:
-            user_collection.insert_one(records)
-            return render(request, "Login.html", {})
-        except DuplicateKeyError:
-            err_msg = "The email address is already registered. Please use a different email."
-            print("-+-+-+-+-+-+-+-+-+-+-+- :- ", err_msg)
-            return render(request, "SignUp.html", {"error": err_msg})
-        except Exception as e:
-            print("-=-=-=-=-=-=-=-=-=-=-=- Somthing want wrong :- ", e)
-            err_msg = "An unexpected error occurred. Please try again."
-            return render(request, "SighUp.html", {"error": err_msg})
-
-    return render(request, "SignUp.html", {})
-
-
-def login(request):
-    if request.method == 'POST':
-
-        email = request.POST['user_email']
-        otp = request.POST['otp']
-
-        print("+-+-+-+-+-+ Email from Send OTP Form :-", email)
-        user = user_collection.find_one({"user_email": email})
-
-        # print("User details :- ", user)
-
-        if user:
-            if user.get("otp") == otp:
-                print("*=*=*=*=*=*=*=*=*=*=*=* :- login successfully..")
-                return redirect("/Index")
-            else:
-                print("+-+-+-+-+-+-+-+-+-+-+-+ :- OTP is incorrect.")
-                return render(request, "Login.html", {"visibility": 'block', "email": email, "error": "Invalid OTP."})
-
-    return render(request, "Login.html", {})
-
-
-from django.core.mail import send_mail
-import random
-
-
-def sendotp(request):
-    if request.method == 'POST':
-        otp = random.randint(1000, 9999)
-
-        email = request.POST['user_email']
-
-        print("*=*=*=*=*=*=* otp:- ",otp,"email:- ",email)
-
-        user = user_collection.update_one(
-            {"user_email": email},  # Filter: Match the email
-            {"$set": {"otp": str(otp)}}  # Update: Add or update the OTP field
-        )
-
-        if user.matched_count == 0:
-            err_msg = "Email not found. Please SignUp before requesting an OTP."
-            print("-=-=-=-=-=-=-=-", err_msg)
-            return render(request, "SignUp.html", {"error": err_msg})
-
-        subject = 'Your SnapForge Portal OTP'
-        message = 'Dear user, you have attempted to login in the SnapForge Portal.\n\nUse OTP: ' + str(otp) + '\n\nNote: Do not share the OTP with anyone else. '
-        email_from = settings.EMAIL_HOST_USER
-        recipient_list = [email, ]
-
-        send_mail(subject, message, email_from, recipient_list)
-        print("+=+=+=+=+=+=+=+=+=+  " + email + "  +=+=+=+  Email has been sent successfully...")
-
-        print(f"*=*=*=*=*=*=*=*=*=*=*=*  OTP {otp} successfully added to the collection for email {email}")
-
-    return render(request, "Login.html", {"visibility": 'block', "email": email})
 
 # import matplotlib.pyplot as plt
 # import matplotlib.image as mpimg
